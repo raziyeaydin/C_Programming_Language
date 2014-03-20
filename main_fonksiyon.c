37a00725e93bda091f852a5191fa9e144c2a00bc
Raziye AYDIN <raziye.aydin@bil.omu.edu.tr>
2014-03-21 01:22:31 +0200

    Create main_fonksiyon.c

---
+++ main_fonksiyon.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main0(void){
+  printf("Her sey benle baslar.");
+	return 0;
+}
+int main(void){
+	printf("Her seyin baslangici benim.");
+	return 0;
+}
+int main1(void){
+	printf("Kimse benden once baslayamaz.");
+	return 0;
+}
+int fonksiyon(void){
+	printf("Ben bu yarisa katilmiyorum.");
+	return 0;
+}
