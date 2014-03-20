6d18c90c54a95ea41fd552337f0dfb214e585c5e
Raziye AYDIN <raziye.aydin@bil.omu.edu.tr>
2014-03-21 01:16:52 +0200

    Create bosluk_silme2.c

---
+++ bosluk_silme2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void){
+    char cumle;
+    int bosluk;
+    bosluk='\0';
+    printf("cumle giriniz: ");
+    scanf("%c",cumle);
+    for( ; ; )
+         if cumle[i]=bosluk {
+            if cumle[i+1]=bosluk
+                cumle[i+1]='';
+            else
+                i++;}
+    getch();
+    return 0;
+} 
